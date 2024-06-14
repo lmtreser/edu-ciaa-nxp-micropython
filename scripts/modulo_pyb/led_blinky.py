7d0f39d0bc5e68010fc7d0327f46ae37813c7738
lmtreser <lmtreser@gmail.com>
2024-06-14 19:03:37 -0300

    update blink

--- scripts/modulo_pyb/led_blinky.py
+++ scripts/modulo_pyb/led_blinky.py
@@ -1,10 +1,10 @@
 import pyb
 
-led1 = pyb.LED(1)
+led1 = pyb.LED(3)
 pyb.delay(5000)
 
 while True:
     led1.on()
-    pyb.delay(1000)
+    pyb.delay(100)
     led1.off()
-    pyb.delay(1000)
+    pyb.delay(100)
